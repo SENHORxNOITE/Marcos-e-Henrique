93113d689231fbc00c1dda4cb5e0777a21cc610f
SENHORxNOITE <gbhausae@gmail.com>
2023-03-08 02:48:59 +0900

    Project's Initial Commit

---
+++ www/components/motor.js
@@ -0,0 +1,9 @@
+function calcularconsumo() {   
+     var altura, peso, consumo;  
+       altura = document.getElementById('altura').value   
+      peso = document.getElementById('peso').value   
+       
+       consumo = peso / (altura * altura)
+        
+    document.getElementById("nome").innerHTML = consumo + "IMC"
+    }
