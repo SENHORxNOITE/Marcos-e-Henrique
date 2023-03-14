5c700f03a9623abbd060c8dc5a38aa1b1ea83ab7
SENHORxNOITE <gbhausae@gmail.com>
2023-03-15 05:11:11 +0900

    aaaa

--- www/components/motor.js
+++ www/components/motor.js
@@ -1,9 +1,20 @@
-function calcularconsumo() {   
-     var altura, peso, consumo;  
-       altura = document.getElementById('altura').value   
-      peso = document.getElementById('peso').value   
-       
-       consumo = peso / (altura * altura)
-        
-    document.getElementById("nome").innerHTML = consumo + "IMC"
-    }
+function calcularconsumo(){
+ 
+ var km = document.getElementById ('km').value;
+ var gas = document.getElementById('gas').value;
+
+ var media = km / gas;
+
+if(media >20){
+ document.getElementById("extremamente").innerHTML = "<h3><=20km/l | Extremamente Econômico</h3>"
+} 
+else if((media >=12) && (media <20)) {
+    document.getElementById("economico").innerHTML = "<h3><=12km/l---->20km/l | Econômico</h3>"
+}
+else if((media >=8) && (media <12)) {
+    document.getElementById("normal").innerHTML = "<h3><=8km/l---->12km/l | Normal</h3>"
+}
+else{
+    document.getElementById("alto").innerHTML = "<h3>>8km/l | Alto consumo</h3>"
+}
+}
